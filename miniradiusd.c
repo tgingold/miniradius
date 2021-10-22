dcd4dfea9ccf2bb4362acd493ce5a8223c3cec4e
Tristan Gingold <tgingold@free.fr>
2021-10-22 07:14:07 +0200

    More dumps.

--- miniradiusd.c
+++ miniradiusd.c
@@ -10,6 +10,9 @@
 #include "hmac_md5.h"
 #include "md5.h"
 
+static unsigned port = 1812;
+static unsigned dump_eap_only = 1;
+
 /* Radius: RFC 2865
    https://www.rfc-editor.org/rfc/rfc2865
 
@@ -92,9 +95,105 @@
 #define EAP_TYPE_MD5_CHALLENGE 4
 #define EAP_TYPE_OTP 5
 #define EAP_TYPE_GTC 6
+#define EAP_TYPE_EAP_TLS 13
+#define EAP_TYPE_EAP_TTLS 0x15
+#define EAP_TYPE_PEAP 0x19
+#define EAP_TYPE_PEAPv0 33
+#define EAP_TYPE_PWD 52  /* RFC 5931 */
 
 static unsigned char key[] = "pass";
 static unsigned key_len = sizeof(key) - 1;
+
+static const char *
+disp_tls_content_type (unsigned c)
+{
+  switch (c) {
+  case 20:
+    return "change_cipher_spec";
+  case 21:
+    return "alert";
+  case 22:
+    return "handshake";
+  case 23:
+    return "application_data";
+  default:
+    return "??";
+  }
+}
+
+/* TLS 1.2:
+   https://datatracker.ietf.org/doc/html/rfc5246
+*/
+static void
+dump_tls (const unsigned char *p, unsigned plen)
+{
+  unsigned len;
+  printf ("   type: %02x %s", p[0], disp_tls_content_type (p[0]));
+  printf (",  protocol version: %u.%u", p[1], p[2]);
+  len = (p[3] << 8) | p[4];
+  printf (",  len: %u\n", len);
+}
+
+/* See:
+   https://tools.ietf.org/id/draft-josefsson-pppext-eap-tls-eap-06.txt
+*/
+static void
+dump_peap (const unsigned char *p, unsigned plen)
+{
+  unsigned len;
+  unsigned hlen;
+
+  hlen = 1;
+  printf ("   flags: %02x", p[0]);
+  if (p[0] & 0x80)
+    printf (" Len");
+  if (p[0] & 0x40)
+    printf (" MoreFrag");
+  if (p[0] & 0x20)
+    printf (" PEAP-Start");
+  if (p[0] & 0x18)
+    printf (" MBZ-Err");
+  printf (", ver: %u", p[0] & 0x7);
+  if (p[0] & 0x80) {
+    len = (p[1] << 24) | (p[2] << 16) | (p[3] << 8) | p[4];
+    printf (",  len=%u", len);
+    hlen += 4;
+  }
+  putchar('\n');
+  if (plen > hlen)
+    dump_tls (p + hlen, plen - hlen);
+}
+
+static const char *
+disp_eap_type (unsigned char typ)
+{
+  switch (typ) {
+  case EAP_TYPE_IDENTITY:
+    return "identity";
+  case EAP_TYPE_NOTIFICATION:
+    return "notification";
+  case EAP_TYPE_NAK:
+    return "Nak";
+  case EAP_TYPE_MD5_CHALLENGE:
+    return "MD5-Challenge";
+  case EAP_TYPE_OTP:
+    return "One-Time Password (OTP)";
+  case EAP_TYPE_GTC:
+    return "Generic Token Card";
+  case EAP_TYPE_EAP_TLS:
+    return "EAP-TLS";
+  case EAP_TYPE_EAP_TTLS:
+    return "EAP-TTLS";
+  case EAP_TYPE_PEAP:
+    return "PEAP";
+  case EAP_TYPE_PEAPv0:
+    return "PEAPv0";
+  case EAP_TYPE_PWD:
+    return "pwd";
+  default:
+    return "??";
+  }
+}
 
 static void
 dump_eap_message (const unsigned char *p, unsigned plen)
@@ -121,44 +220,36 @@ dump_eap_message (const unsigned char *p, unsigned plen)
   case EAP_CODE_RESPONSE:
     {
       if (len <= 4) {
-	printf (" Incorrect length");
+	printf (" Incorrect length\n");
 	break;
       }
-      printf ("  type: %u  ", p[4]);
+      printf ("  type: %u  %s\n", p[4], disp_eap_type(p[4]));
       switch (p[4]) {
-      case EAP_TYPE_IDENTITY:
-	printf ("identity");
-	break;
-      case EAP_TYPE_NOTIFICATION:
-	printf ("notification");
+      case EAP_TYPE_PEAP:
+	dump_peap (p + 5, len - 5);
 	break;
       case EAP_TYPE_NAK:
-	printf ("Nak");
-	break;
-      case EAP_TYPE_MD5_CHALLENGE:
-	printf ("MD5-Challenge");
-	break;
-      case EAP_TYPE_OTP:
-	printf ("One-Time Password (OTP)");
-	break;
-      case EAP_TYPE_GTC:
-	printf ("Generic Token Card");
-	break;
-      default:
-	printf ("??");
-	break;
+	{
+	  unsigned i;
+	  printf ("   desired types:");
+	  for (i = 5; i < len; i++)
+	    printf ("  %02x (%s)", p[i], disp_eap_type(p[i]));
+	  putchar('\n');
+	}
       }
     }
     break;
   default:
+    putchar('\n');
     break;
   }
-  putchar('\n');
 }
 
-static void
+  static void
 dump_radius (const unsigned char *p, unsigned plen)
 {
+  unsigned char eap_buf[4096];
+  unsigned eap_len = 0;
   unsigned len;
   unsigned off;
   unsigned i;
@@ -204,10 +295,12 @@ dump_radius (const unsigned char *p, unsigned plen)
     return;
   }
 
-  printf ("Authenticator:");
-  for (i = 0; i < 16; i++)
-    printf (" %02x", p[4 + i]);
-  printf ("\n");
+  if (!dump_eap_only) {
+    printf ("Authenticator:");
+    for (i = 0; i < 16; i++)
+      printf (" %02x", p[4 + i]);
+    printf ("\n");
+  }
 
   for (off = 20; off < plen; ) {
     unsigned atype;
@@ -219,170 +312,183 @@ dump_radius (const unsigned char *p, unsigned plen)
     }
     atype = p[off];
     alen = p[off + 1];
-    printf ("attribute: %u, len: %u  ", atype, alen);
 
-    switch (atype)
-      {
-      case ATTR_USER_NAME:
-	printf("User-Name");
-	break;
-      case ATTR_USER_PASSWORD:
-	printf("User-Password");
-	break;
-      case ATTR_CHAP_PASSWORD:
-	printf("CHAP-Password");
-	break;
-      case ATTR_NAS_IP_ADDRESS:
-	printf("NAS-IP-Address");
-	break;
-      case ATTR_NAS_PORT:
-	printf("NAS-Port");
-	break;
-      case ATTR_SERVICE_TYPE:
-	printf("Service-Type");
-	break;
-      case ATTR_FRAMED_PROTOCOL:
-	printf("Framed-Protocol");
-	break;
-      case ATTR_FRAMED_IP_ADDRESS:
-	printf("Framed-IP-Address");
-	break;
-      case ATTR_FRAMED_IP_NETMASK:
-	printf("Framed-IP-Netmask");
-	break;
-      case ATTR_FRAMED_ROUTING:
-	printf("Framed-Routing");
-	break;
-      case ATTR_FILTER_ID:
-	printf("Filter-Id");
-	break;
-      case ATTR_FRAMED_MTU:
-	printf("Framed-MTU");
-	break;
-      case ATTR_FRAMED_COMPRESSION:
-	printf("Framed-Compression");
-	break;
-      case ATTR_LOGIN_IP_HOST:
-	printf("Login-IP-Host");
-	break;
-      case ATTR_LOGIN_SERVICE:
-	printf("Login-Service");
-	break;
-      case ATTR_LOGIN_TCP_PORT:
-	printf("Login-TCP-Port");
-	break;
-      case ATTR_REPLY_MESSAGE:
-	printf("Reply-Message");
-	break;
-      case ATTR_CALLBACK_NUMBER:
-	printf("Callback-Number");
-	break;
-      case ATTR_CALLBACK_ID:
-	printf("Callback-Id");
-	break;
-      case ATTR_FRAMED_ROUTE:
-	printf("Framed-Route");
-	break;
-      case ATTR_FRAMED_IPX_NETWORK:
-	printf("Framed-IPX-Network");
-	break;
-      case ATTR_STATE:
-	printf("State");
-	break;
-      case ATTR_CLASS:
-	printf("Class");
-	break;
-      case ATTR_VENDOR_SPECIFIC:
-	printf("Vendor-Specific");
-	break;
-      case ATTR_SESSION_TIMEOUT:
-	printf("Session-Timeout");
-	break;
-      case ATTR_IDLE_TIMEOUT:
-	printf("Idle-Timeout");
-	break;
-      case ATTR_TERMINATION_ACTION:
-	printf("Termination-Action");
-	break;
-      case ATTR_CALLED_STATION_ID:
-	printf("Called-Station-Id");
-	break;
-      case ATTR_CALLING_STATION_ID:
-	printf("Calling-Station-Id");
-	break;
-      case ATTR_NAS_IDENTIFIER:
-	printf("NAS-Identifier");
-	break;
-      case ATTR_PROXY_STATE:
-	printf("Proxy-State");
-	break;
-      case ATTR_LOGIN_LAT_SERVICE:
-	printf("Login-LAT-Service");
-	break;
-      case ATTR_LOGIN_LAT_NODE:
-	printf("Login-LAT-Node");
-	break;
-      case ATTR_LOGIN_LAT_GROUP:
-	printf("Login-LAT-Group");
-	break;
-      case ATTR_FRAMED_APPLETALK_LINK:
-	printf("Framed-AppleTalk-Link");
-	break;
-      case ATTR_FRAMED_APPLETALK_NETWORK:
-	printf("Framed-AppleTalk-Network");
-	break;
-      case ATTR_FRAMED_APPLETALK_ZONE:
-	printf("Framed-AppleTalk-Zone");
-	break;
-      case ATTR_CHAP_CHALLENGE:
-	printf("CHAP-Challenge");
-	break;
-      case ATTR_NAS_PORT_TYPE:
-	printf("NAS-Port-Type");
-	break;
-      case ATTR_PORT_LIMIT:
-	printf("Port-Limit");
-	break;
-      case ATTR_LOGIN_LAT_PORT:
-	printf("Login-LAT-Port");
-	break;
-      case ATTR_CONNECT_INFO:
-	printf("Connect-Info");
-	break;
-      case ATTR_EAP_MESSAGE:
-	printf("EAP-Message");
-	break;
-      case ATTR_MESSAGE_AUTHENTICATOR:
-	printf("Message-Authenticator");
-	break;
-      default:
-	printf ("??");
-      }
+    if (!dump_eap_only) {
+      printf ("attribute: %u, len: %u  ", atype, alen);
+
+      switch (atype)
+	{
+	case ATTR_USER_NAME:
+	  printf("User-Name");
+	  break;
+	case ATTR_USER_PASSWORD:
+	  printf("User-Password");
+	  break;
+	case ATTR_CHAP_PASSWORD:
+	  printf("CHAP-Password");
+	  break;
+	case ATTR_NAS_IP_ADDRESS:
+	  printf("NAS-IP-Address");
+	  break;
+	case ATTR_NAS_PORT:
+	  printf("NAS-Port");
+	  break;
+	case ATTR_SERVICE_TYPE:
+	  printf("Service-Type");
+	  break;
+	case ATTR_FRAMED_PROTOCOL:
+	  printf("Framed-Protocol");
+	  break;
+	case ATTR_FRAMED_IP_ADDRESS:
+	  printf("Framed-IP-Address");
+	  break;
+	case ATTR_FRAMED_IP_NETMASK:
+	  printf("Framed-IP-Netmask");
+	  break;
+	case ATTR_FRAMED_ROUTING:
+	  printf("Framed-Routing");
+	  break;
+	case ATTR_FILTER_ID:
+	  printf("Filter-Id");
+	  break;
+	case ATTR_FRAMED_MTU:
+	  printf("Framed-MTU");
+	  break;
+	case ATTR_FRAMED_COMPRESSION:
+	  printf("Framed-Compression");
+	  break;
+	case ATTR_LOGIN_IP_HOST:
+	  printf("Login-IP-Host");
+	  break;
+	case ATTR_LOGIN_SERVICE:
+	  printf("Login-Service");
+	  break;
+	case ATTR_LOGIN_TCP_PORT:
+	  printf("Login-TCP-Port");
+	  break;
+	case ATTR_REPLY_MESSAGE:
+	  printf("Reply-Message");
+	  break;
+	case ATTR_CALLBACK_NUMBER:
+	  printf("Callback-Number");
+	  break;
+	case ATTR_CALLBACK_ID:
+	  printf("Callback-Id");
+	  break;
+	case ATTR_FRAMED_ROUTE:
+	  printf("Framed-Route");
+	  break;
+	case ATTR_FRAMED_IPX_NETWORK:
+	  printf("Framed-IPX-Network");
+	  break;
+	case ATTR_STATE:
+	  printf("State");
+	  break;
+	case ATTR_CLASS:
+	  printf("Class");
+	  break;
+	case ATTR_VENDOR_SPECIFIC:
+	  printf("Vendor-Specific");
+	  break;
+	case ATTR_SESSION_TIMEOUT:
+	  printf("Session-Timeout");
+	  break;
+	case ATTR_IDLE_TIMEOUT:
+	  printf("Idle-Timeout");
+	  break;
+	case ATTR_TERMINATION_ACTION:
+	  printf("Termination-Action");
+	  break;
+	case ATTR_CALLED_STATION_ID:
+	  printf("Called-Station-Id");
+	  break;
+	case ATTR_CALLING_STATION_ID:
+	  printf("Calling-Station-Id");
+	  break;
+	case ATTR_NAS_IDENTIFIER:
+	  printf("NAS-Identifier");
+	  break;
+	case ATTR_PROXY_STATE:
+	  printf("Proxy-State");
+	  break;
+	case ATTR_LOGIN_LAT_SERVICE:
+	  printf("Login-LAT-Service");
+	  break;
+	case ATTR_LOGIN_LAT_NODE:
+	  printf("Login-LAT-Node");
+	  break;
+	case ATTR_LOGIN_LAT_GROUP:
+	  printf("Login-LAT-Group");
+	  break;
+	case ATTR_FRAMED_APPLETALK_LINK:
+	  printf("Framed-AppleTalk-Link");
+	  break;
+	case ATTR_FRAMED_APPLETALK_NETWORK:
+	  printf("Framed-AppleTalk-Network");
+	  break;
+	case ATTR_FRAMED_APPLETALK_ZONE:
+	  printf("Framed-AppleTalk-Zone");
+	  break;
+	case ATTR_CHAP_CHALLENGE:
+	  printf("CHAP-Challenge");
+	  break;
+	case ATTR_NAS_PORT_TYPE:
+	  printf("NAS-Port-Type");
+	  break;
+	case ATTR_PORT_LIMIT:
+	  printf("Port-Limit");
+	  break;
+	case ATTR_LOGIN_LAT_PORT:
+	  printf("Login-LAT-Port");
+	  break;
+	case ATTR_CONNECT_INFO:
+	  printf("Connect-Info");
+	  break;
+	case ATTR_EAP_MESSAGE:
+	  printf("EAP-Message");
+	  break;
+	case ATTR_MESSAGE_AUTHENTICATOR:
+	  printf("Message-Authenticator");
+	  break;
+	default:
+	  printf ("??");
+	}
+    }
     if (alen < 3 || off + alen > plen) {
       printf (" - bad attribute length\n");
       return;
     }
-    printf ("\n");
-    printf (" value:");
-    for (i = 2; i < alen; i++)
-      printf (" %02x", p[off + i]);
-    printf ("\n");
-    printf (" ascii: ");
-    for (i = 2; i < alen; i++) {
-      unsigned c = p[off + i];
-      if (c < ' ' || c >= 127)
-	c = '.';
-      putchar(c);
+
+    if (!dump_eap_only) {
+      printf ("\n");
+      printf (" value:");
+      for (i = 2; i < alen; i++)
+	printf (" %02x", p[off + i]);
+      printf ("\n");
+      printf (" ascii: ");
+      for (i = 2; i < alen; i++) {
+	unsigned c = p[off + i];
+	if (c < ' ' || c >= 127)
+	  c = '.';
+	putchar(c);
+      }
+      printf ("\n");
     }
-    printf ("\n");
 
     if (atype == ATTR_EAP_MESSAGE) {
       if (alen < 6)
 	printf ("  Bad EAP message length\n");
-      else
-	dump_eap_message(p + off + 2, alen - 2);
+      else {
+	memcpy (eap_buf + eap_len, p + off + 2, alen - 2);
+	eap_len += alen - 2;
+      }
     }
     off += alen;
+  }
+
+  if (eap_len != 0) {
+    printf ("EAP message:\n");
+    dump_eap_message(eap_buf, eap_len);
   }
 }
 
@@ -416,22 +522,14 @@ auth_request(unsigned char *p, int plen)
   return 0;
 }
 
+
 static unsigned int
-compute_eap_authenticator (unsigned char *rep, unsigned int len)
+compute_eap_authenticator_noalloc (unsigned char *rep, unsigned int len,
+				   unsigned char *p_mac)
 {
   struct MD5Context md5_ctxt;
-  unsigned char *r = rep + len;
-  unsigned char *p_mac;
-
-  /* Message authenticator.  */
-  r[0] = ATTR_MESSAGE_AUTHENTICATOR;
-  r[1] = 18;
-  p_mac = r + 2;
-  memset (p_mac, 0, 16);
-  r += r[1];
 
   /* RADIUS packet length.  */
-  len = r - rep;
   rep[3] = len & 0xff;
   rep[2] = len >> 8;
 
@@ -475,6 +573,22 @@ compute_eap_authenticator (unsigned char *rep, unsigned int len)
   return len;
 }
 
+static unsigned int
+compute_eap_authenticator (unsigned char *rep, unsigned int len)
+{
+  unsigned char *r = rep + len;
+  unsigned char *p_mac;
+
+  /* Message authenticator.  */
+  r[0] = ATTR_MESSAGE_AUTHENTICATOR;
+  r[1] = 18;
+  p_mac = r + 2;
+  memset (p_mac, 0, 16);
+  r += r[1];
+
+  return compute_eap_authenticator_noalloc(rep, r - rep, p_mac);
+}
+
 static int
 do_eap_challenge(unsigned char *req, int rea_len,
 		 unsigned char *eap, int eap_len,
@@ -482,6 +596,7 @@ do_eap_challenge(unsigned char *req, int rea_len,
 {
   unsigned char *r;
   unsigned len;
+  unsigned char *p_mac;
 
   /* Header.  */
   rep[0] = CODE_ACCESS_CHALLENGE;
@@ -496,12 +611,6 @@ do_eap_challenge(unsigned char *req, int rea_len,
      Copy proxy, create state... */
   r = rep + 20;
 
-  r[0] = ATTR_STATE;
-  r[1] = 4;
-  r[2] = 4;
-  r[3] = 7;
-  r += r[1];
-
   r[0] = ATTR_EAP_MESSAGE;
   r[1] = 0;
   r[2] = EAP_CODE_REQUEST;
@@ -513,17 +622,60 @@ do_eap_challenge(unsigned char *req, int rea_len,
     memcpy (r + 7, "otp-md5 487 dog2 ", 17);
     r[5] = 4 + 1 + 17;
   }
-  else {
+  else if (0) {
     r[6] = EAP_TYPE_MD5_CHALLENGE;
     r[7] = 8;  /* value size */
     memcpy (r + 8, "\x12\x34\x56\x78\x9a\xbc\xde\xf0", 8);  /* value */
     memcpy (r + 16, "MRname", 6);
     r[5] = 4 + 2 + 8 + 6;
   }
+  else if (1) {
+    r[6] = EAP_TYPE_PEAP;
+    r[7] = 0x20; /* start */
+    r[5] = 4 + 2;
+  }
+  else if (0) {
+    r[6] = EAP_TYPE_EAP_TTLS;
+    r[7] = 0x20;  /* start */
+    r[5] = 4 + 2;
+  }
+  else if (1) {
+    r[6] = EAP_TYPE_EAP_TLS;
+    r[7] = 0x20;  /* start */
+    r[5] = 4 + 2;
+  }
   r[1] = r[5] + 2;
   r += r[1];
 
-  len = compute_eap_authenticator(rep, r - rep);
+  /* Auth */
+  r[0] = ATTR_MESSAGE_AUTHENTICATOR;
+  r[1] = 18;
+  p_mac = r + 2;
+  memset (p_mac, 0, 16);
+  r += r[1];
+
+  /* State */
+  r[0] = ATTR_STATE;
+  r[1] = 18;
+  r[ 2] = 0x59;
+  r[ 3] = 0x35;
+  r[ 4] = 0xa8;
+  r[ 5] = 0x59;
+  r[ 6] = 0x59;
+  r[ 7] = 0x34;
+  r[ 8] = 0xa5;
+  r[ 9] = 0xd2;
+  r[10] = 0x70;
+  r[11] = 0x91;
+  r[12] = 0x10;
+  r[13] = 0xd2;
+  r[14] = 0xbd;
+  r[15] = 0x37;
+  r[16] = 0xa5;
+  r[17] = 0x5e;
+  r += r[1];
+
+  len = compute_eap_authenticator_noalloc(rep, r - rep, p_mac);
 
   return len;
 }
@@ -641,7 +793,6 @@ server(void)
 {
   int sock;
   struct sockaddr_in myaddr;
-  unsigned port = 1812;
   int bin_log;
 
   sock = socket (PF_INET, SOCK_DGRAM, 0);
@@ -718,7 +869,7 @@ server(void)
       write(bin_log, rep, res);
 
     r = sendto (sock, rep, res, 0, (struct sockaddr *)&addr, alen);
-    if (r < 0)
+    if (r != res)
       perror ("sendto");
   }
 }
@@ -731,26 +882,127 @@ test_server(void)
   int len;
   int res;
 
-  len = read(0, buf, sizeof buf);
-  if (len < 0) {
-    perror ("read");
+  while (1) {
+    len = read(0, buf, 4);
+    if (len != 4) {
+      perror ("read");
+      return 1;
+    }
+
+    len = (buf[2] << 8) | buf[3];
+    res = read(0, buf + 4, len - 4);
+    if (res != len - 4) {
+      perror ("read");
+      return 1;
+    }
+
+    printf ("## Read %u bytes ########\n", len);
+    dump_radius(buf, len);
+
+    if (buf[0] == CODE_ACCESS_REQUEST)
+      res = handle_access_request(buf, len, rep);
+    else
+      res = 0;
+
+    if (res > 0) {
+      printf ("## Write %u bytes ########\n", res);
+      dump_radius(rep, res);
+      //    write(1, rep, res);
+    }
+  }
+
+  return 0;
+}
+
+static int
+dump_pcap(void)
+{
+  unsigned char buf[4096];
+  int len;
+
+  len = read(0, buf, 24);
+  if (len != 24) {
+    perror ("read hdr");
+    return 1;
+  }
+  if (buf[3] != 0xa1 || buf[2] != 0xb2 || buf[1] != 0xc3 || buf[0] != 0xd4) {
+    printf ("Bad pcap magic\n");
     return 1;
   }
 
-  printf ("## Read %u bytes ########\n", len);
-  dump_radius(buf, len);
+  while (1) {
+    unsigned plen;
+    unsigned flen;
+    unsigned hlen;
+    unsigned char *p;
 
-  if (buf[0] == CODE_ACCESS_REQUEST)
-    res = handle_access_request(buf, len, rep);
-  else
-    res = 0;
+    /* Read per-packet header.  */
+    len = read(0, buf, 16);
+    if (len == 0)
+      return 0;
+    if (len != 16) {
+      perror ("read phdr");
+      return 1;
+    }
+    plen = buf[8] | (buf[9] << 8) | (buf[10] << 16) | (buf[11] << 24);
+    flen = buf[12] | (buf[13] << 8) | (buf[14] << 16) | (buf[15] << 24);
 
-  if (res > 0) {
-    printf ("## Write %u bytes ########\n", res);
-    dump_radius(rep, res);
-    //    write(1, rep, res);
+    len = read(0, buf, plen);
+    if (len != plen) {
+      perror("read pkt");
+      return 1;
+    }
+
+    hlen = 6 + 6 + 2 + 20 + 8;  /* mac, ip, udp.  */
+
+    printf ("## packet len=%u", plen);
+    if (0) {
+      p = buf;
+      unsigned i, j;
+      for (i = 0; i < plen; i += 16) {
+	printf ("%04x:", i);
+	for (j = i; j < plen && j < i + 16; j++)
+	  printf (" %02x", p[j]);
+	putchar('\n');
+      }
+    }
+
+    if (plen < hlen) {
+      printf (" truncated\n");
+      continue;
+    }
+    if (buf[12] != 0x08 || buf[13] != 0x00) {
+      printf (" not IP\n");
+      continue;
+    }
+    if ((buf[14] & 0xf0) != 0x40
+	|| (buf[14] & 0x0f) < 5) {
+      printf (" not IPv4\n");
+      continue;
+    }
+
+    hlen = 14;
+    if (buf[hlen + 9] != 0x11) {
+      printf (" not UDP\n");
+      continue;
+    }
+
+    hlen += 4 * (buf[hlen] & 0x0f);
+    if (((buf[hlen + 0] << 8) | buf[hlen + 1]) == port)
+      printf (" from server\n");
+    else if (((buf[hlen + 2] << 8) | buf[hlen + 3]) == port)
+      printf (" to server\n");
+    else {
+      printf (" not radius port (hlen=%u, port=%04x)\n", hlen, port);
+      continue;
+    }
+
+    hlen += 8;
+    p = buf + hlen;
+    plen -= hlen;
+
+    dump_radius(p, plen);
   }
-  return 0;
 }
 
 int
@@ -758,6 +1010,11 @@ main (int argc, char *argv[])
 {
   if (argc == 2 && strcmp(argv[1], "-") == 0)
     return test_server();
+  else if (argc == 2 && strcmp(argv[1], "-r") == 0)
+    return dump_pcap();
   else
     return server();
 }
+
+
+// ZhAXDptRAUGNPa86ZhZL3Jjdz1lDXay031fHddv17vz0Tw3P40tqQDFNsnYy8Lr
